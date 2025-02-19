c8bb4c4dc5bcebed7fa708c68e5a55c7ec619e6b
Boris Doesborg <boris.doesborg@finalist.nl>
2025-02-19 18:58:49 +0100

    wip

---
+++ web/modules/custom/side_migration/side_migration.post_update.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Post-update hook to execute a migration.
+ */
+function side_migration_post_update_run_migration(&$sandbox) {
+  // The ID of the migration to execute.
+    $migration_id = 'taxonomy_term_tags';
+
+  // Load the migration plugin.
+  $migration = \Drupal::service('plugin.manager.migration')->createInstance($migration_id);
+
+  if ($migration) {
+    // Create a migration executable.
+    $executable = new \Drupal\migrate\MigrateExecutable(
+      $migration,
+      new \Drupal\migrate\MigrateMessage()
+    );
+
+    // Execute the migration.
+    $executable->import();
+  }
+  else {
+    throw new \Exception("Migration with ID $migration_id not found.");
+  }
+}
